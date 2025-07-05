db960bcade60a56d93917f3d0d0c1646425f0729
TanjinaHasan15 <tanzinahasantanzinahasan@gmail.com>
2025-07-05 10:07:09 +0600

    import_data

--- diabetes-linear-regression.py
+++ diabetes-linear-regression.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 
+
+df = pd.read_csv("diabetes.csv")
